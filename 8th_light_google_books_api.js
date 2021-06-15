7c8497e33d5d628b3a21ccd1231e64fc081cb631
fosterhorak <foster.f.horak@gmail.com>
2021-06-15 05:37:36 -0500

    planning

--- 8th_light_google_books_api.js
+++ 8th_light_google_books_api.js
@@ -11,3 +11,65 @@
 // For programming language, choose any language you want as long as it is not the same language you chose to review in the Code Review section above. Feel free to use a library (or not) for the Google Books call or JSON parsing.
 
 // Please do not add any additional features.
+
+
+
+// planning:
+
+// program will initialize the following:
+    //  - empty user reading list
+    //  - empty (temp/resuable) search results list (include books author, title, publishing company)
+        // this list will be filled with the top 5 results from google books api with search query is made
+        // when user selects one, the info will be copied into the user library reading list
+
+
+
+// user experience / flow
+
+// welcome message w/ instructions
+    // [upon starting program]
+    // "Welcome to Your Library!!"
+
+    // "[Main Menu]"
+        // You currently have {readingList.length} books in your Reading List.
+
+        // New Book Search: type "new" + enter
+        // Reading List: type "list" + enter
+        // Main Menu = type "main" + enter
+        // Quit = type "q" + enter
+            // NOTE - include error catching for invalid entries
+    
+
+    // "[Reading List]"
+        // note = will display current reading list in a table
+        // [#      title      author      publishin company]
+        
+        // New Book Search: type "new" + enter
+        // Reading List: type "list" + enter
+        // Main Menu = type "main" + enter
+        // Quit = type "q" + enter
+            // NOTE - include error catching for invalid entries
+
+    // "[New Book Search]""
+        // Please type your search query and press enter...
+            // how to let user escape from search function?? 
+                // -- require them to enter "s:" + search term, then keep other commands active?
+                // provide a key word to escape search?
+            // any sort of error catching or edge cases that need to be caught here??
+            // anything special that needs to be done to query to work with API??
+            // if there are no results, provide guidance and allow user to try again
+
+        
+    // "[Book Search Results]"
+        // will return top 5 results from google books api (numbered 1-5)
+        // below search results will provide guidance...
+            // To add a book to your reading list: type "# of your book selection" + enter 
+                // note - you'll be redirected to your reading list
+
+        // New Book Search: type "new" + enter
+        // Reading List: type "list" + enter
+        // Main Menu = type "main" + enter
+        // Quit = type "q" + enter
+            // NOTE - include error catching for invalid entries
+
+                
